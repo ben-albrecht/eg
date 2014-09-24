e8313728ac2dc71e2862165a1e61d4546a28244c
ben <benalbreck@gmail.com>
2014-09-24 17:58:43 -0400

    Working Inter-mation, but buggy plots.py for general usage

--- python/matplotlib/Inter-mation.py
+++ python/matplotlib/Inter-mation.py
@@ -1,64 +1,92 @@
-import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import animation
-from matplotlib.widgets import Slider, Button, RadioButtons
-from pylab import *
+import pylab as pyl
+import matplotlib.pyplot as plt
+import matplotlib.widgets as wig
+#from matplotlib.widgets import Slider, Button, RadioButtons
 
-# fig = plt.figure()
-ax = plt.axes(xlim=(0, 2), ylim(-2,2))
-# ax = subplot(111)
-subplots_adjust(left=0.25, bottom=0.25)
-t = arange(0.0, 1.0, 0.001)
-a0 = 5
-f0 = 3
-s = a0*sin(2*pi*f0*t)
-# l, = plot(t, s, lw=2, color='red')
+## Sin Wave Plot ##
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 1), ylim=(-10, 10))
 l, = ax.plot([], [], lw=2, color='red')
-axis([0, 1, -10, 10])
 
+fig.add_subplot(111)
+fig.subplots_adjust(left=0.25, bottom=0.25)
+
+#t = arange(0.0, 1.0, 0.001)
+## Amplitude of sin wave
+a0 = 5
+## Phase of sin wave
+f0 = 3
+## Sin wave equation applied to entire range, t
+##s = a0*sin(2*pi*f0*t)
+## `l,` is a plot of t vs s, with adjusted line width and color
+## Save this plot as l, for later use
+##l, = plot(t,s, lw=2, color='red')
+#l, = plot([], [], lw=2, color='red')
+#
+### Slider Plot ##
+## Define dimensions of a new plot
+#axis([0, 1, -10, 10])
+
+
+# We need to somehow put these on a subplot of some sort
+# New axis color
 axcolor = 'lightgoldenrodyellow'
-axfreq = axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
-axamp  = axes([0.25, 0.15, 0.65, 0.03], axisbg=axcolor)  
+# Define positions of sliders
+axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
+axamp  = plt.axes([0.25, 0.15, 0.65, 0.03], axisbg=axcolor)
 
-sfreq = Slider(axfreq, 'Freq', 0.1, 30.0, valinit=f0)
-samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0)
+# Define Sliders, ranges, and initial values
+sfreq = wig.Slider(axfreq, 'Freq', 0.1, 30.0, valinit=f0)
+samp =  wig.Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0)
+
 
 def update(val):
     amp = samp.val
     freq = sfreq.val
-    l.set_ydata(amp*sin(2*pi*freq*t))
-    draw()
+    #l.set_ydata(amp*np.sin(2*np.pi*freq*t))
+    # draw()
 sfreq.on_changed(update)
 samp.on_changed(update)
 
-resetax = axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
+resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+button = wig.Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
+
 
 def reset(event):
     sfreq.reset()
     samp.reset()
 button.on_clicked(reset)
 
-rax = axes([0.025, 0.5, 0.15, 0.15], axisbg=axcolor)                                                                                  
-radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)                                                                         
-def colorfunc(label):                                                                                                                 
-    l.set_color(label)                                                                                                                
-    draw()                                                                                                                            
-radio.on_clicked(colorfunc)                                                                                                           
-                                                                                                                                      
+rax = plt.axes([0.025, 0.5, 0.15, 0.15], axisbg=axcolor)
+radio = wig.RadioButtons(rax, ('red', 'blue', 'green'), active=0)
+
+
+def colorfunc(label):
+    l.set_color(label)
+    plt.draw()
+radio.on_clicked(colorfunc)
+
 def init():
     l.set_data([], [])
     return l,
 
+import numpy as np
+
 def animate(i):
+    amp = samp.val
+    freq = sfreq.val
     x = np.linspace(0, 2, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i)
+    y = amp * np.sin(2 * np.pi * (x - freq * i))
+    #y = a0 * np.sin(2 * np.pi *( x - f0 * i))
     l.set_data(x, y)
     return l,
 
-anim = animation.FuncAnimation(fig, animate, init_func=init,                                                                          
-                               frames=200, interval=20,                                                                               
-                               blit=True) 
+from matplotlib import animation
 
-plt.show() 
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=200, interval=200,
+                               blit=True)
 
+
+plt.show()
