d34be4e87c0d9b440589ffd23fdc165f954783ac
Ben <benalbreck@gmail.com>
2015-01-26 10:45:49 -0500

    Matplotlib examples

--- python/Libraries/matplotlib/plots.py
+++ python/Libraries/matplotlib/plots.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
 
-# Create plot along points y=1,2,3
-plt.plot([1,2,3])
+def main():
+    # Create plot along points y=1,2,3
+    plt.plot([1,2,3])
 
-# Create subplot with 2x1 (row x col) dimensions, labeled plot 1
-plt.subplot(211)
+    # Create subplot with 2x1 (row x col) dimensions, labeled plot 1
+    plt.subplot(211)
 
-# Set x range of plot ro 12
-plt.plot(range(12))
+    # Set x range of plot ro 12
+    plt.plot(range(12))
 
-# Create a 2nd subplot, 2x1 dimensions, labeled as plot 2 (below plot 1)
-# background of plot will be yellow
-plt.subplot(212, axisbg='y') 
+    # Create a 2nd subplot, 2x1 dimensions, labeled as plot 2 (below plot 1)
+    # background of plot will be yellow
+    plt.subplot(212, axisbg='y')
 
-# show the plots
-plt.show()
+    # show the plots
+    plt.show()
 
+if __name__ == '__main__':
+    main()
