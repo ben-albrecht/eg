531b836f5d50d34f1d456b66470fcbedb403bf8a
Ben Albrecht <benalbrecht@pitt.edu>
2015-11-04 19:40:28 -0800

    Changes output rules and thus parsing rules

--- cpp/Concepts/general/parse_python/parse.cpp
+++ cpp/Concepts/general/parse_python/parse.cpp
@@ -8,32 +8,23 @@
 
 int main()
 {
-    // Call chplenv.py
+    // Call chplenv.py and collect output into string
     std::string command = "python chplenv.py 2>&1";
     std::string output = exec(command);
-    //printf("str: %s\n", output.c_str());
 
-    // Parse output
-    std::string delimiter = "\n";
-    size_t pos = 0;
-    std::string token;
+    // Parse output of chplenv.py and populate map with env vars
+    std::map<std::string, const char*> env = populateMap(output);
 
-    while ((pos = output.find(delimiter)) != std::string::npos)
+    // Iterate over and print key/value pairs in map out for proof that it worked!
+    for( std::map<std::string, const char*>::iterator ii=env.begin(); ii!=env.end(); ++ii)
     {
-
-        token = output.substr(0, pos);
-        std::cout << token << std::endl;
-        output.erase(0, pos + delimiter.length());
-
+        std::cout << (*ii).first << ": " << (*ii).second << std::endl;
     }
-
-    // Feed output into map
-    std::map<std::string, const char*> env;
 
     return 0;
 }
 
-std::string exec(std::string cmd) 
+std::string exec(std::string cmd)
 {
     FILE* pipe = popen(cmd.c_str(), "r");
     if (!pipe) return "ERROR";
@@ -46,3 +37,45 @@ std::string exec(std::string cmd)
     pclose(pipe);
     return result;
 }
+
+
+std::map<std::string, const char*> populateMap(std::string output)
+{
+    // Destructively parses string output for environment variables (keys)
+    // and their values according to printchplenv (values), and returns an
+    // std::map populated with the key/value pairs
+
+    std::map<std::string, const char*> env;
+
+    // Lines
+    std::string line= "";
+    std::string lineDelimiter = "\n";
+    size_t linePos = 0;        // Line break position
+
+    // Tokens
+    std::string tokenDelimiter = "=";
+    size_t delimiterPos = 0;    // Position of delimiter
+    size_t valuePos = 0;        // Position of value
+
+    std::string key = "";
+    std::string value = "";
+
+    while ((linePos = output.find(lineDelimiter)) != std::string::npos)
+    {
+        line = output.substr(0, linePos);
+
+        // Key is substring up until "=" on a given line
+        delimiterPos = line.find(tokenDelimiter);
+        key = line.substr(0, delimiterPos);
+
+        // value is substring up after "=" on a given line
+        valuePos = delimiterPos + tokenDelimiter.length();
+        value = line.substr(valuePos);
+
+        // Populate map, duplicating string, because output is destroyed in memory
+        env[key] = strdup(value.c_str());
+        output.erase(0, linePos + lineDelimiter.length());
+    }
+    return env;
+}
+
--- cpp/Concepts/general/parse_python/parse.h
+++ cpp/Concepts/general/parse_python/parse.h
@@ -2,9 +2,11 @@
 #define PARSE_H 1
 
 #include <string>
+#include <map>
 
 int main();
 
 std::string exec(std::string cmd);
 
+std::map<std::string, const char*> populateMap(std::string);
 #endif
